fa2eb0a67097230821e96548710896a04a87011e
Paul Hecker <paul@iwascoding.com>
2015-10-14 15:29:37 +0200

    Initial import

---
+++ GSAppleScriptTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GSAppleScriptTest
+//
+//  Created by Paul Hecker on 14.10.15.
+//  Copyright © 2015 Paul Hecker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GSAppleScriptTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GSAppleScriptTest
+//
+//  Created by Paul Hecker on 14.10.15.
+//  Copyright © 2015 Paul Hecker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
